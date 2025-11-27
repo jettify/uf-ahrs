0c528ba080904552794eece45c60f955e7b45543
Nikolay Novik <nickolainovik@gmail.com>
2025-11-26 22:57:40 -0500

    feat: Add new ahrs trait.

---
+++ src/traits.rs
@@ -0,0 +1,16 @@
+use nalgebra::{Scalar, UnitQuaternion, Vector3};
+
+pub trait Ahrs {
+    fn update(
+        &mut self,
+        gyroscope: &Vector3<f32>,
+        accelerometer: &Vector3<f32>,
+        magnetometer: &Vector3<f32>,
+    ) -> &UnitQuaternion<f32>;
+    fn update_imu(
+        &mut self,
+        gyroscope: &Vector3<f32>,
+        accelerometer: &Vector3<f32>,
+    ) -> &UnitQuaternion<f32>;
+    fn update_gyro(&mut self, gyroscope: &Vector3<f32>) -> &UnitQuaternion<f32>;
+}
