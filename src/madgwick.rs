b732f42285001d7730b25d5b3beb80985fe093b6
Nikolay Novik <nickolainovik@gmail.com>
2025-12-19 19:46:31 -0500

    chore: Cleanup rebase issues.

--- src/madgwick.rs
+++ src/madgwick.rs
@@ -67,7 +67,7 @@ impl Ahrs for Madgwick {
         let h = q * (Quaternion::from_parts(0.0, mag) * q.conjugate());
         let b = Quaternion::new(0.0, Vector2::new(h[0], h[1]).norm(), 0.0, h[2]);
 
-        let F = Vector6::new(
+        let f = Vector6::new(
             2.0 * (q[0] * q[2] - q[3] * q[1]) - accel[0],
             2.0 * (q[3] * q[0] + q[1] * q[2]) - accel[1],
             2.0 * (half - q[0] * q[0] - q[1] * q[1]) - accel[2],
@@ -81,7 +81,7 @@ impl Ahrs for Madgwick {
                 - mag[2],
         );
 
-        let J_t = Matrix6::new(
+        let j_t = Matrix6::new(
             -2.0 * q[1],
             2.0 * q[0],
             0.0,
@@ -120,7 +120,7 @@ impl Ahrs for Madgwick {
             0.0,
         );
 
-        let Some(step) = (J_t * F).try_normalize(f32::EPSILON) else {
+        let Some(step) = (j_t * f).try_normalize(f32::EPSILON) else {
             return self.update_gyro(gyroscope);
         };
         let q_dot = q * Quaternion::from_parts(0.0, gyroscope) * half
@@ -142,14 +142,14 @@ impl Ahrs for Madgwick {
             return self.update_gyro(gyroscope);
         };
 
-        let F = Vector4::new(
+        let f = Vector4::new(
             2.0 * (q[0] * q[2] - q[3] * q[1]) - accel[0],
             2.0 * (q[3] * q[0] + q[1] * q[2]) - accel[1],
             2.0 * (half - q[0] * q[0] - q[1] * q[1]) - accel[2],
             0.0,
         );
 
-        let J_t = Matrix4::new(
+        let j_t = Matrix4::new(
             -2.0 * q[1],
             2.0 * q[0],
             0.0,
@@ -167,7 +167,7 @@ impl Ahrs for Madgwick {
             0.0,
             0.0,
         );
-        let Some(step) = (J_t * F).try_normalize(f32::EPSILON) else {
+        let Some(step) = (j_t * f).try_normalize(f32::EPSILON) else {
             return self.update_gyro(gyroscope);
         };
         let q_dot = (q * Quaternion::from_parts(0.0, gyroscope)) * half
