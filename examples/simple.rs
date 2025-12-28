b651549c478cd821d0f49aeed8f67a0536287073
jettify <nickolainovik@gmail.com>
2025-12-28 15:48:33 -0500

    Add simple example, and minor cleanup. (#10)

---
+++ examples/simple.rs
@@ -0,0 +1,29 @@
+use core::time::Duration;
+use nalgebra::Vector3;
+use uf_ahrs::{Ahrs, Madgwick, MadgwickParams, Mahony, MahonyParams, Vqf, VqfParameters};
+
+fn main() {
+    let dt = Duration::from_secs_f32(1.0 / 100.0);
+
+    let mut mahony = Mahony::new(dt, MahonyParams::default());
+    let mut madgwick = Madgwick::new(dt, MadgwickParams::default());
+    let mut vqf = Vqf::new(dt, VqfParameters::default());
+
+    // Sensor data
+    let gyr = Vector3::new(0.0, 0.0, 0.0);
+    let acc = Vector3::new(0.0, 0.0, 9.81);
+    let mag = Vector3::new(20.0, 0.0, 0.0);
+
+    mahony.update(gyr, acc, mag);
+    madgwick.update(gyr, acc, mag);
+    vqf.update(gyr, acc, mag);
+
+    // Get orientation as UnitQuaternion
+    let q_mahony = mahony.orientation();
+    let q_madgwick = madgwick.orientation();
+    let q_vqf = vqf.orientation();
+
+    println!("Mahony: {:#?}", q_mahony.euler_angles());
+    println!("Mahony: {:#?}", q_madgwick.euler_angles());
+    println!("Mahony: {:#?}", q_vqf.euler_angles());
+}
